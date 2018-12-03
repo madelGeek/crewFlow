6d827b8debe6b099309e2256bdabc23249b10171
madelgeek <daria.gorchylina@gmail.com>
2018-12-03 14:19:20 +0200

    Added missed file: package.json, package-lock.json, App.tsx

---
+++ app/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Root } from './root.component';
+
+ReactDOM.render(<Root />,
+    document.getElementById('root'));
+
